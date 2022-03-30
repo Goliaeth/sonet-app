d2c07e26a405e1e081c15b5fddd13fa0e8874611
Nick <goliaeth@gmail.com>
2022-03-30 11:11:30 +0500

    start using css modules in react

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,18 @@
+import classes from './Sidebar.module.css'
+
 const Sidebar = () => {
   return (
-    <nav className="sidebar">
-      <div>
-        <a href="#s">Profile</a>
+    <nav className={classes.sidebar}>
+      <div className={classes.link}>
+        <a>Profile</a>
       </div>
-      <div className="link-text">
+      <div className="sidebarLink">
         <a href="#s">Messages</a>
       </div>
-      <div className="link-text">
+      <div className="sidebarLink">
         <a href="#s">News</a>
       </div>
-      <div className="link-text">
+      <div className="sidebarLink">
         <a href="#s">Settings</a>
       </div>
     </nav>
